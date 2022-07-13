914b334a9682704a5bebf609743930cf684f40ee
Brian Wangora <brianwangora@gmail.com>
2022-07-13 17:03:58 +0300

    tests passed

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,22 @@
-// Code Keypad Component Here
+import { render } from "@testing-library/react";
+import React from "react";
+
+function Keypad () {
+    function handleChange (event) {
+        event.preventDefault();
+        console.log("Entering password...");
+    }
+
+    return(
+        <div>
+            <input
+            type="password"
+            name="password"
+            onChange={handleChange}
+            placeholder="ENTER PASSWORD"/>
+        </div>
+    )  
+}
+
+
+export default Keypad
